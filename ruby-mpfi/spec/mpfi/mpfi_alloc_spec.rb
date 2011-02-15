4224e0533f03b6e91887cbcb9176f2bc56575561
Takayuki YAMAGUCHI <d@ytak.info>
2011-02-15 13:31:19 +0900

    MPFI: Add some tests of MPFI.new.

--- ruby-mpfi/spec/mpfi/mpfi_alloc_spec.rb
+++ ruby-mpfi/spec/mpfi/mpfi_alloc_spec.rb
@@ -49,3 +49,46 @@ describe MPFI, "when initializing MPFI instance by method interval" do
 
 end
 
+describe MPFI, "when setting value from some objects" do
+  before(:all) do
+    MPFR.set_default_prec(128)
+    MPFR.set_error_of_test(MPFR('1e-12'))
+  end
+
+  it "should create an MPFI object from a string" do
+    n = MPFI("[1.2, 1.3]")
+    n.left.check_error(1.2).should be_true
+    n.right.check_error(1.3).should be_true
+  end
+
+  it "should create an MPFI object from an integer" do
+    n = MPFI(8)
+    n.left.check_error(8).should be_true
+    n.right.check_error(8).should be_true
+  end
+
+  it "should create an MPFI object from an array of float numbers" do
+    n = MPFI([-1.3, -0.5])
+    n.left.check_error(-1.3)
+    n.right.check_error(-0.5)
+  end
+
+  it "should create an MPFI object from an array of MPFR numbers" do
+    n = MPFI([MPFR(-1.0), MPFR(-0.5)])
+    n.left.check_error(-1.0)
+    n.right.check_error(-0.5)
+  end
+
+  it "should create an MPFI object from an array of strings" do
+    n = MPFI(['3.8', '4.9'])
+    n.left.check_error(3.8)
+    n.right.check_error(4.9)
+  end
+
+  it "should create an MPFI object from an array of some objects" do
+    n = MPFI([MPFR(1.2), '4.9'])
+    n.left.check_error(1.2)
+    n.right.check_error(4.9)
+  end
+
+end
