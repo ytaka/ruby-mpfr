f73ef24e5a9bdd59bcdeb55e4c678a02c4dc6576
Takayuki YAMAGUCHI <d@ytak.info>
2011-02-17 13:48:48 +0900

    Bug fix of check_error for Matrix and Vector.

--- ruby-mpfr/lib/mpfr/rspec.rb
+++ ruby-mpfr/lib/mpfr/rspec.rb
@@ -15,20 +15,20 @@ class MPFR
 
   class Matrix
     def check_error(other, error = nil)
+      err = error || @error
       column_size.times.all? do |i|
         row_siize.times.all? do |j|
-          self[i][j].check_error(other[i][j], error)
+          self[i][j].check_error(other[i][j], err)
         end
       end
-      err = error || @error
-      column
     end
   end
 
   module Vector
     def check_error(other, error = nil)
+      err = error || @error
       size.times.all? do |i|
-        self[i].check_error(other[i], error)
+        self[i].check_error(other[i], err)
       end
     end
   end
